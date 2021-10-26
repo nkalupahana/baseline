0e1ccf8da1b3460583f9b97a8fa0706fe6327749
nkalupahana <avishkank@gmail.com>
2021-10-26 00:11:53 -0500

    Basic cloud function for mood logging

--- functions/index.js
+++ functions/index.js
@@ -1,9 +1,48 @@
 const functions = require("firebase-functions");
+const admin = require("firebase-admin");
+admin.initializeApp();
 
-// // Create and Deploy Your First Cloud Functions
-// // https://firebase.google.com/docs/functions/write-firebase-functions
-//
-// exports.helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+const validateAuth = async (req, res) => {
+    if ((!req.headers.authorization || !req.headers.authorization.startsWith("Bearer ")) && !(req.cookies && req.cookies.__session)) {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    let idToken;
+    if (req.headers.authorization && req.headers.authorization.startsWith("Bearer ")) {
+        idToken = req.headers.authorization.split("Bearer ")[1];
+    } else if (req.cookies) {
+        idToken = req.cookies.__session;
+    } else {
+        res.status(403).send("Unauthorized");
+        return;
+    }
+
+    try {
+        const decodedIdToken = await admin.auth().verifyIdToken(idToken);
+        req.user = decodedIdToken;
+        return;
+    } catch (error) {
+        console.log(error);
+        res.status(403).send("Unauthorized");
+        return;
+    }
+};
+
+exports.moodLog = functions.https.onRequest(async (req, res) => {
+    res.set('Access-Control-Allow-Origin', "*");
+    res.set('Access-Control-Allow-Methods', 'POST');
+    res.set('Access-Control-Allow-Headers', 'Authorization');
+
+    // Preflight? Stop here.
+    if (req.method === "OPTIONS") {
+        res.status(204).send("");
+        return;
+    }
+
+    await validateAuth(req, res);
+    if (!req.user) return;
+    console.log(req.body);
+    await admin.database().ref(`/${req.user.user_id}/logs`).push(req.body.data);
+    res.send(200);
+});
