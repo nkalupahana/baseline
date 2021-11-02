e1e4378c3e2baaed7a7b2ca40bdf91a5506243a2
nkalupahana <avishkank@gmail.com>
2021-11-02 17:11:55 -0500

    [feat] timer for startup redirect to journal

--- src/lifecycle.js
+++ src/lifecycle.js
@@ -1,10 +1,29 @@
 import history from "./history";
 
-function check() {
-    if (!window.location.pathname.startsWith("/journal")) {
-        history.push("/journal");
-    }
+const THRESHOLD_SEC = 60 * 3;
+
+function getTime() {
+    return Math.round(Date.now() / 1000);
 }
 
-document.addEventListener("resume", check);
-document.addEventListener("deviceready", check);
+function setLastOpenedTime() {
+    window.localStorage.setItem("lastOpenedTime", getTime());
+}
+
+function redirect() {
+    if (!window.location.pathname.startsWith("/journal")) {
+        let lastOpenedTime = window.localStorage.getItem("lastOpenedTime");
+        if (lastOpenedTime !== null) {
+            lastOpenedTime = Number(lastOpenedTime);
+            if (getTime() - lastOpenedTime > THRESHOLD_SEC) {
+                history.push("/journal");
+            }
+        }
+    }
+
+    setLastOpenedTime();
+}
+
+document.addEventListener("resume", redirect);
+document.addEventListener("deviceready", redirect);
+document.addEventListener("pause", setLastOpenedTime);
