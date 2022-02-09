31fff330c6187256a74cdf3722e238fb1d61dee5
nkalupahana <avishkank@gmail.com>
2022-02-09 15:17:02 -0600

    Generalize MoodLogList for use in the MonthSummary

--- src/components/Summary/Week/WeekSummary.jsx
+++ src/components/Summary/Week/WeekSummary.jsx
@@ -1,7 +1,7 @@
 import ldb from "../../../db";
 import { useLiveQuery } from "dexie-react-hooks";
 import SummaryHeader from "../SummaryHeader";
-import MoodLogList from "../MoodLogList";
+import WeekMoodLogList from "./WeekMoodLogList";
 import WeekMoodGraph from "./WeekMoodGraph";
 import { useState } from "react";
 import Preloader from "../../../pages/Preloader";
@@ -32,7 +32,7 @@ const WeekSummary = ({ gettingData, setMenuDisabled }) => {
         <div className="week-summary-grid">
             <SummaryHeader style={{ gridArea: "heading" }}></SummaryHeader>
             { logs && logs.length > 0 && <WeekMoodGraph requestedDate={requestedDate} setRequestedDate={setRequestedDate} logs={logs}></WeekMoodGraph> }
-            { logs && logs.length > 0 && <MoodLogList setMenuDisabled={setMenuDisabled} style={{"gridArea": "logs"}} logs={logs} requestedDate={requestedDate} setRequestedDate={setRequestedDate}></MoodLogList> }
+            { logs && logs.length > 0 && <WeekMoodLogList setMenuDisabled={setMenuDisabled} style={{"gridArea": "logs"}} logs={logs} requestedDate={requestedDate} setRequestedDate={setRequestedDate} /> }
             { logs && logs.length === 0 && !gettingData && <p className="text-center">Write your first mood log by clicking on the pencil in the bottom right!</p> }
             { (!logs || (logs.length === 0 && gettingData)) && <Preloader /> }
         </div>
