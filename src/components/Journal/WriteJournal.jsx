a96fff81edb054ef1ec036f0f19619e1b10c1428
nkalupahana <avishkank@gmail.com>
2022-03-10 14:30:28 -0500

    [backlog] removed onBlur change from Write -> Finish

--- src/components/Journal/WriteJournal.jsx
+++ src/components/Journal/WriteJournal.jsx
@@ -3,17 +3,10 @@ import { IonTextarea } from "@ionic/react";
 import { useEffect } from "react";
 import history from "../../history";
 import { signOutAndCleanUp } from "../../firebase";
-import { Capacitor } from "@capacitor/core";
 
 const WriteJournal = ({ setMoodRead, moodWrite, ...props }) => {
     const next = () => {
         history.push("/journal/finish");
-    };
-
-    const loseFocus = () => {
-        if (props.text.trim() && Capacitor.getPlatform() !== "web") {
-            next();
-        }
     };
 
     useEffect(() => {
@@ -24,7 +17,7 @@ const WriteJournal = ({ setMoodRead, moodWrite, ...props }) => {
         <div className="center-journal">
             <div className="title" onClick={signOutAndCleanUp}>What's happening?</div>
             <p className="text-center bold" onClick={next}>If you don't want to write right now, tap here to jump to mood logging.</p>
-            <IonTextarea autocapitalize="sentences" autofocus auto-grow={true} className="tx" value={props.text} placeholder="Start typing here!" onIonBlur={loseFocus} onIonChange={e => props.setText(e.detail.value)}/>
+            <IonTextarea autocapitalize="sentences" autofocus auto-grow={true} className="tx" value={props.text} placeholder="Start typing here!" onIonChange={e => props.setText(e.detail.value)}/>
             { props.text.trim() && <div onClick={next} className="fake-button">Continue</div> }
             <br />
         </div>
