6663e67526dde3c81ceed38c6591eea5dafee337
nkalupahana <avishkank@gmail.com>
2022-06-22 12:05:59 -0700

    [backlog] nicer line break on mobile title

--- src/components/Summary/Week/WeekSummary.tsx
+++ src/components/Summary/Week/WeekSummary.tsx
@@ -33,7 +33,7 @@ const WeekSummary = ({ setMenuDisabled, logs } : Props) => {
     return (
         <div className="week-summary-grid" style={(logs && logs.length > 0) ? {} : {"height": "100%"}}>
             <div style={{ gridArea: "heading" }} className="center-summary">
-                <div className="title">Here's how your week has been looking.</div>
+                <div className="title"><span>Here's how your week has</span> <span>been looking.</span></div>
             </div>
             { logs && logs.length > 0 && <>
                 <WeekMoodGraph requestedDate={requestedDate} setRequestedDate={setRequestedDate} logs={logs}></WeekMoodGraph>
