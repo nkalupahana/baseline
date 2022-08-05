0fab191ecaede22c6b621be15a125e832622c45c
nkalupahana <avishkank@gmail.com>
2022-08-05 14:48:03 -0700

    Limit Android phone-sized devices to portrait

--- android/app/src/main/java/app/getbaseline/baseline/MainActivity.java
+++ android/app/src/main/java/app/getbaseline/baseline/MainActivity.java
@@ -1,5 +1,17 @@
 package app.getbaseline.baseline;
 
+import android.content.pm.ActivityInfo;
+import android.os.Bundle;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        
+        if (getResources().getBoolean(R.bool.portrait_only)) {
+            setRequestedOrientation(ActivityInfo.SCREEN_ORIENTATION_PORTRAIT);
+        }
+    }
+}
