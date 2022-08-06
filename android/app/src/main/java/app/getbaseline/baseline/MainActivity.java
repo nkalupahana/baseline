067fa567f1926b57a697e5e1ad5d32c0391250e6
nkalupahana <avishkank@gmail.com>
2022-08-05 18:58:04 -0700

    Dark mode now working on more Android phones

--- android/app/src/main/java/app/getbaseline/baseline/MainActivity.java
+++ android/app/src/main/java/app/getbaseline/baseline/MainActivity.java
@@ -1,5 +1,21 @@
 package app.getbaseline.baseline;
 
+import android.content.res.Configuration;
+import android.os.Bundle;
+import android.webkit.WebSettings;
+
 import com.getcapacitor.BridgeActivity;
 
-public class MainActivity extends BridgeActivity {}
+public class MainActivity extends BridgeActivity {
+    @Override
+    public void onStart() {
+        super.onStart();
+        int nightModeFlags = getResources().getConfiguration().uiMode & Configuration.UI_MODE_NIGHT_MASK;
+        WebSettings webSettings = this.bridge.getWebView().getSettings();
+        if (nightModeFlags == Configuration.UI_MODE_NIGHT_YES) {
+            if (android.os.Build.VERSION.SDK_INT >= android.os.Build.VERSION_CODES.Q) {
+                webSettings.setForceDark(WebSettings.FORCE_DARK_ON);
+            }
+        }
+    }
+}
