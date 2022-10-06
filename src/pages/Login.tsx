87f9682a3cbb2616c5c58e775f6e6882a0d855ae
nkalupahana <avishkank@gmail.com>
2022-10-06 18:20:35 -0500

    Source and Campaign reporting working for iOS

--- src/pages/Login.tsx
+++ src/pages/Login.tsx
@@ -260,7 +260,9 @@ const Login = ({ setLoggingIn } : { setLoggingIn: (_: boolean) => void }) => {
             await makeRequest("syncUserInfo", auth.currentUser!, {
                 offset: DateTime.now().offset,
                 fcmToken: token.token,
-                deviceId: (await Device.getId()).uuid
+                deviceId: (await Device.getId()).uuid,
+                utm_source: localStorage.getItem("utm_source"),
+                utm_campaign: localStorage.getItem("utm_campaign"),
             });
             await FirebaseMessaging.subscribeToTopic({ topic: "all" });
         } catch {}
