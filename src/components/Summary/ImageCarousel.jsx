34286aee04195f4eb58e4f2da9b5d7b183921536
nkalupahana <avishkank@gmail.com>
2022-10-09 02:48:36 -0500

    Basic image download ready

--- src/components/Summary/ImageCarousel.jsx
+++ src/components/Summary/ImageCarousel.jsx
@@ -4,10 +4,14 @@ import Flickity from "react-flickity-component";
 import "flickity/dist/flickity.min.css";
 import "flickity-fullscreen/fullscreen.css";
 import "flickity-fullscreen/fullscreen";
+import "flickity-download/download.css";
+import "flickity-download/download";
 import useCallbackRef from "../../useCallbackRef";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { BASE_URL, checkKeys } from "../../helpers";
 import { getIdToken } from "firebase/auth";
+import { Capacitor } from "@capacitor/core";
+import { Media } from "@getbaseline/capacitor-community-media";
 
 const ImageCarousel = ({ files, setInFullscreen }) => {
     const [accessURLs, setAccessURLs] = useState([]);
@@ -46,6 +50,18 @@ const ImageCarousel = ({ files, setInFullscreen }) => {
         }
 
         node.on("fullscreenChange", listener);
+        node.nativeDownload = dataurl => {
+            if (Capacitor.getPlatform() === "web") {
+                const link = document.createElement("a");
+                link.href = dataurl;
+                link.download = "image.webp";
+                link.click();
+            } else {
+                Media.savePhoto({
+                    path: dataurl
+                });
+            }
+        };
 
         return () => {
             node.off("fullscreenChange", listener);
@@ -54,7 +70,7 @@ const ImageCarousel = ({ files, setInFullscreen }) => {
 
     return (
         <>
-            { accessURLs && <Flickity flickityRef={flkty} className="carousel-mods" reloadOnUpdate={false} options={{"wrapAround": true, "adaptiveHeight": true, "fullscreen": true}}>
+            { accessURLs && <Flickity flickityRef={flkty} className="carousel-mods" reloadOnUpdate={false} options={{"wrapAround": true, "adaptiveHeight": true, "fullscreen": true, "download": true}}>
                 { accessURLs.map(url => <div key={url} style={{"display": "flex", "alignItems": "center", "justifyContent": "center"}}><img alt="User-attached for mood log" src={url} /></div>)}
             </Flickity> }
         </>
