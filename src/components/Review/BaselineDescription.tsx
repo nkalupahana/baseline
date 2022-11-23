5d7ce218aaa9a5c4c1eeb65d4bafdaab59dfe381
nkalupahana <avishkank@gmail.com>
2022-11-23 11:59:26 -0800

    Add Happo screenshot of component

---
+++ src/components/Review/BaselineDescription.tsx
@@ -0,0 +1,24 @@
+import { BASELINE_EXP } from "../../data";
+
+const BaselineDescription = () => {
+    return (
+        <>
+            <p>
+                Mental health isn't just (or even primarily) about clinical diagnoses. Much more of it is simply about living a happy,
+                healthy life. {BASELINE_EXP} (And if you don't see a trend, don't worry about your baseline just yet.)
+            </p>
+            <p>
+                Notice your baseline falling? You might want to make a conscious effort to bring more things into your life that you
+                typically associate with higher mood scores. If your baseline has fallen significantly, or isn't rebounding, you should to
+                take some time to reflect on what might've changed in your life that led to that.
+            </p>
+            <p>
+                Mood logging is a great start to that process &mdash; we recommend that you review your mood logs to look for any changes.
+                And of course, professional help is the best way to really figure out what's going on. Check out our help resources in the
+                main menu for more information.
+            </p>
+        </>
+    );
+};
+
+export default BaselineDescription;
