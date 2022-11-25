6e0df8b03dd949cdeeb6a7d0aeff073e9372d474
nkalupahana <avishkank@gmail.com>
2022-11-25 14:26:34 -0800

    move to scheduled-services

--- scheduled-services/main.tf
+++ scheduled-services/main.tf
@@ -12,8 +12,8 @@ module "service-accounts" {
     source          = "terraform-google-modules/service-accounts/google"
     version         = "4.1.1"
     project_id      = var.project
-    display_name    = "bi-microservices"
-    names           = ["bi-microservices"]
-    description     = "Service account for bi microservices (tf managed)"
+    display_name    = "scheduled-services"
+    names           = ["scheduled-services"]
+    description     = "Service account for scheduled services (tf managed)"
     project_roles   = ["${var.project}=>roles/bigquery.admin"]
 }
--- scheduled-services/src/index.ts
+++ scheduled-services/src/index.ts
@@ -8,6 +8,11 @@ app.post("/", (req, res) => {
     res.send(200);
 });
 
+app.post("/path2", (req, res) => {
+    console.log(req.body);
+    res.send(200);
+});
+
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
     console.log("Listening on port", port);
