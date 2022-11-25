593e6cd1ee911f3b2ef87bb6b15bf5b0c0fd41d0
nkalupahana <avishkank@gmail.com>
2022-11-25 14:06:37 -0800

    add basic bi container

---
+++ bi/src/index.ts
@@ -0,0 +1,14 @@
+import express from "express";
+
+const app = express();
+app.use(express.json());
+
+app.post("/", (req, res) => {
+    console.log(req.body);
+    res.send(200);
+});
+
+const port = process.env.PORT || 8080;
+app.listen(port, () => {
+    console.log("Listening on port", port);
+});
