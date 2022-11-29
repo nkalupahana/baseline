5a0902fb657cc77d1ca74362196bd7d79e21c605
nkalupahana <avishkank@gmail.com>
2022-11-28 18:38:01 -0800

    actually, nope

--- scheduled-services/main.tf
+++ scheduled-services/main.tf
@@ -95,7 +95,7 @@ resource "google_cloud_scheduler_job" "bi_and_retention_messaging" {
 }
 
 resource "google_service_account_iam_member" "give-perms-to-gh-actions" {
-  service_account_id = "projects/${var.project}/serviceAccounts/github-action-420733850@iam.gserviceaccount.com"
+  service_account_id = "projects/${var.project}/serviceAccounts/github-action-420733850@getbaselineapp.iam.gserviceaccount.com"
   role               = "roles/iam.serviceAccountAdmin"
   member             = "serviceAccount:${module.service-accounts.email}"
 }
