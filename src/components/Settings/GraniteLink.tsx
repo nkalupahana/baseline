1e29198fa82182f5d1804ce8ad792a40080157d6
nkalupahana <avishkank@gmail.com>
2023-03-18 13:37:58 -0500

    Switch to plugin, and turn on listening for connection status

--- src/components/Settings/GraniteLink.tsx
+++ src/components/Settings/GraniteLink.tsx
@@ -3,23 +3,39 @@ import { useEffect, useState } from "react";
 import Preloader from "../../pages/Preloader";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../firebase";
-import { get, ref } from "firebase/database";
+import { DataSnapshot, off, onValue, ref } from "firebase/database";
+import { OAuth2Client } from "@byteowls/capacitor-oauth2";
+import { toast } from "../../helpers";
 
 const GraniteLink = () => {
     const [user] = useAuthState(auth);
     const [linkData, setLinkData] = useState<boolean | undefined>(undefined);
     useEffect(() => {
         if (!user) return;
-        (async () => {
-            const v = await get(ref(db, `/${user.uid}/partners/granite`));
-            setLinkData(!!v.val());
-        })();
+        const listener = async (snap: DataSnapshot) => {
+            setLinkData(!!snap.val());
+        }
+        const graniteRef = ref(db, `/${user.uid}/partners/granite`)
+        onValue(graniteRef, listener);
+
+        return () => {
+            off(graniteRef, "value", listener);
+        };
     }, [user]);
 
-    const graniteAccess = () => {
+    const graniteAccess = async () => {
         const redirect_uri = `${window.location.origin}/granite/callback`;
         localStorage.setItem("redirect_uri", redirect_uri);
-        window.location.href = `https://granite-labs.herokuapp.com/oauth2/authorize?client_id=76217571-ce1c-4492-bd0f-7c4d2aeb0026&redirect_uri=${redirect_uri}`;
+        try {
+            await OAuth2Client.authenticate({
+                appId: "76217571-ce1c-4492-bd0f-7c4d2aeb0026",
+                authorizationBaseUrl: "https://granite-labs.herokuapp.com/oauth2/authorize",
+                redirectUrl: redirect_uri
+            });
+        } catch (e: any) {
+            toast(`Connection error, please try again. (${e.message})`);
+            throw e; // surfaces to Sentry
+        }
     };
 
     return <>
