517847a36f8a00fb1812acd4cd586cebc61e51f1
nkalupahana <avishkank@gmail.com>
2023-04-06 16:30:33 -0500

    First pass of Granite Access (works on iOS and web)

--- src/components/Settings/GraniteCallback.tsx
+++ src/components/Settings/GraniteCallback.tsx
@@ -1,38 +1,8 @@
-import { useEffect } from "react";
-import { makeRequest, toast } from "../../helpers";
 import Preloader from "../../pages/Preloader";
 import history from "../../history";
-import { useAuthState } from "react-firebase-hooks/auth";
-import { auth } from "../../firebase";
 
 const GraniteCallback = () => {
-    const [user] = useAuthState(auth);
-
-    useEffect(() => {
-        if (!user) return;
-        (async () => {
-            const params = new URLSearchParams(window.location.search);
-            const code = params.get("code");
-            const redirect_uri = localStorage.getItem("redirect_uri");
-            if (!code || !redirect_uri) {
-                toast("Something went wrong. Please try again.");
-                history.replace("/settings");
-                return;
-            }
-
-            localStorage.removeItem("redirect_uri");
-            const success = await makeRequest("accounts/granite/link", user, {
-                code,
-                redirect_uri
-            });
-
-            if (success) toast("Account linked!", "bottom");
-            history.replace("/settings");
-        })();
-    }, [user]);
-
     const resetFlow = () => {
-        localStorage.removeItem("redirect_uri");
         history.replace("/settings");
     };
 
--- src/components/Settings/GraniteLink.tsx
+++ src/components/Settings/GraniteLink.tsx
@@ -1,19 +1,24 @@
-import { IonButton } from "@ionic/react";
+import { IonSpinner } from "@ionic/react";
 import { useEffect, useState } from "react";
 import Preloader from "../../pages/Preloader";
 import { useAuthState } from "react-firebase-hooks/auth";
 import { auth, db } from "../../firebase";
 import { DataSnapshot, off, onValue, ref } from "firebase/database";
 import { OAuth2Client } from "@byteowls/capacitor-oauth2";
-import { toast } from "../../helpers";
+import { makeRequest, toast } from "../../helpers";
+import hash from "crypto-js/sha256";
+import base64 from "crypto-js/enc-base64url"
+import { Capacitor } from "@capacitor/core";
 
 const GraniteLink = () => {
     const [user] = useAuthState(auth);
+    const [submitting, setSubmitting] = useState(false);
     const [linkData, setLinkData] = useState<boolean | undefined>(undefined);
     useEffect(() => {
         if (!user) return;
         const listener = async (snap: DataSnapshot) => {
             setLinkData(!!snap.val());
+            setSubmitting(false);
         }
         const graniteRef = ref(db, `/${user.uid}/partners/granite`)
         onValue(graniteRef, listener);
@@ -24,22 +29,61 @@ const GraniteLink = () => {
     }, [user]);
 
     const graniteAccess = async () => {
-        const redirect_uri = `${window.location.origin}/granite/callback`;
-        localStorage.setItem("redirect_uri", redirect_uri);
+        setSubmitting(true);
+        let redirectUrl = "";
+        if (Capacitor.getPlatform() === "web") {
+            redirectUrl = `${window.location.origin}/granite/callback`;
+        } else {
+            redirectUrl = "app.getbaseline.baseline:/";
+        }
+
         try {
-            await OAuth2Client.authenticate({
-                appId: "76217571-ce1c-4492-bd0f-7c4d2aeb0026",
-                authorizationBaseUrl: "https://granite-labs.herokuapp.com/oauth2/authorize",
-                redirectUrl: redirect_uri
+            const code_verifier = crypto.randomUUID() + crypto.randomUUID();
+            const res = await OAuth2Client.authenticate({
+                appId: "baseline",
+                authorizationBaseUrl: "https://granite-platform.herokuapp.com/oidc/auth",
+                accessTokenEndpoint: "https://granite-platform.herokuapp.com/oidc/token",
+                responseType: "code",
+                scope: "openid",
+                pkceEnabled: true,
+                redirectUrl,
+                web: {
+                    accessTokenEndpoint: "",
+                    pkceEnabled: false,
+                    additionalParameters: {
+                        code_challenge: hash(code_verifier).toString(base64),
+                        code_challenge_method: "S256"
+                    }
+                }
             });
+            
+            if (Capacitor.getPlatform() === "web") {
+                await makeRequest("accounts//granite/link", user, {
+                    flow: "code",
+                    code: res.authorization_response.code,
+                    redirect_uri: redirectUrl,
+                    code_verifier
+                }, setSubmitting);
+            } else {
+                await makeRequest("accounts/granite/link", user, {
+                    flow: "token",
+                    access_token: res.access_token_response.access_token,
+                    id_token: res.access_token_response.id_token
+                }, setSubmitting);
+
+            }
         } catch (e: any) {
             toast(`Connection error, please try again. (${e.message})`);
+            setSubmitting(false);
             throw e; // surfaces to Sentry
         }
     };
 
     return <>
-        { linkData === false && <IonButton onClick={graniteAccess}>Connect with GraniteAccess</IonButton> }
+        { linkData === false && <div onClick={graniteAccess} className="finish-button">
+            { !submitting && <>Connect with GraniteAccess</> }
+            { submitting && <IonSpinner className="loader" name="crescent" /> }
+        </div> }
         { linkData === true && <p className="margin-top-0 margin-bottom-0">Linked with Granite!</p> }
         { linkData === undefined && <Preloader message="" /> }
     </>;
