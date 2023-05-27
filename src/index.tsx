344674cfed6f96106e5672a139ee194c0725a376
Nisala Kalupahana <avishkank@gmail.com>
2023-05-27 16:55:29 -0700

    Add Sentry source maps to CI pipeline (#281)
    - [x] Android
    - [x] iOS
    - [x] Code Review

--- src/index.tsx
+++ src/index.tsx
@@ -8,6 +8,8 @@ Sentry.init({
   dsn: "https://ba3d301819cc4156a04140fafa457db3@o4504179120472064.ingest.sentry.io/4504348686286848",
   integrations: [new BrowserTracing()],
   tracesSampleRate: 1.0,
+  release: "development",
+  environment: "development"
 });
 
 ReactDOM.render(
