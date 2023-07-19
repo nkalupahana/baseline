985c1ede401a774e2ceee25b9f8049904df946b6
Nisala Kalupahana <avishkank@gmail.com>
2023-07-19 12:00:29 -0700

    Change requirements for Gap Fund (#296)
    - [ ] iOS
    - [ ] Android
    - [x] Web
    - [x] Code Review

--- backend/src/gap.ts
+++ backend/src/gap.ts
@@ -23,28 +23,35 @@ export const gapFund = async (req: UserRequest, res: Response) => {
         return;
     }
 
-    // Validation
-    for (const key of ["email", "need", "amount", "method"]) {
+    // Input validation
+    for (const key of ["email", "need", "amount", "method", "location", "zone"]) {
         if (typeof body[key] !== "string" || body[key].trim().length === 0 || body[key].length >= 10000) {
-            res.send(400);
+            res.status(400).send("Please update baseline to submit this request.");
             return;
         }
     }
-
-    // Get user statistics for fraud detection
+    
+    // Ensure front-end qualifying validation was not bypassed
     const logRef = db.ref(`/${req.user!.user_id}/logs`);
-    const firstLog = Number(Object.keys(await (await logRef.limitToFirst(1).get()).val())[0]);
 
+    const dayWindow = DateTime.now().setZone(body.zone).endOf("day").minus({ days: 18 });
+    const windowedTimestamps = Object.keys((await (await logRef.orderByKey().startAt(String(dayWindow.toMillis())).get()).val()));
+    let dates = new Set();
+    for (const timestamp of windowedTimestamps) {
+        dates.add(DateTime.fromMillis(Number(timestamp), { zone: body.zone }).toISODate());
+    }
+
+    if (dates.size < 14) {
+        res.status(400).send("Unfortunately, you don't qualify for the Gap Fund yet. Please make sure you've updated baseline, and try again later.");
+        return;
+    }
+
+    // Get user statistics for simple fraud detection
+    const firstLog = Number(Object.keys(await (await logRef.limitToFirst(1).get()).val())[0]);
     const lastLogs = await (await logRef.limitToLast(25).get()).val();
     let lastLogStr = "";
     for (const key in lastLogs) {
         lastLogStr += DateTime.fromMillis(Number(key)).toRFC2822() + "\n";
-    }
-
-    // Simple date validation (real fraud detection will be done manually in the sheet)
-    if (Object.keys(lastLogs).length < 7 || DateTime.now().minus({ days: 5 }).toMillis() < firstLog) {
-        res.send(400);
-        return;
     }
 
     // Put data in user's database
@@ -63,15 +70,16 @@ export const gapFund = async (req: UserRequest, res: Response) => {
     const api = sheets({ version: 'v4', auth });
     await api.spreadsheets.values.append({
         spreadsheetId: "1N3Ecex6TVeWQvd1uKBNtf__XBASx47i5wBktTSbjdus",
-        range: "A2:J9999",
+        range: "A2:K9999",
         valueInputOption: "USER_ENTERED",
         requestBody: {
             values: [[
                 req.user!.user_id, 
-                body.email, 
-                body.need, 
-                body.amount, 
-                body.method, 
+                body.email,
+                body.need,
+                body.amount,
+                body.method,
+                body.location,
                 lastLogStr, // Last log dates
                 DateTime.fromMillis(Number(firstLog)).toRFC2822(), // First log
                 "New", // Status
