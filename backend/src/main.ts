b3b9aa2f775266c74bb2bd8e040da1ee0ba2c1e1
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2023-08-05 23:35:26 -0700

    Something about doing the same thing over and over and expecting different results

--- backend/src/main.ts
+++ backend/src/main.ts
@@ -14,6 +14,7 @@ import { PubSub } from "@google-cloud/pubsub";
 const pubsub = new PubSub();
 
 export const getImage = async (req: UserRequest, res: Response) => {
+    console.log("getImage");
     const db = getDatabase();
 
     const body = req.body;
