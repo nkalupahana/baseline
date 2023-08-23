ba93e30c34a033bbead16edb0201edf288fc7f34
nkalupahana <avishkank@gmail.com>
2023-08-22 19:39:20 -0500

    Try removing video compression for better video

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,6 +1,7 @@
 import { defineConfig } from "cypress";
 
 export default defineConfig({
+  videoCompression: false,
   e2e: {
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
