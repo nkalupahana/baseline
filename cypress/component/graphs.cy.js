40918e507780be0cc11aa00cce3c0ddd4aed0721
nkalupahana <avishkank@gmail.com>
2023-12-27 22:41:33 -0800

    Fix CodeFactor and DeepScan errors

--- cypress/component/graphs.cy.js
+++ cypress/component/graphs.cy.js
@@ -80,9 +80,9 @@ const SEED = 293432490;
 function mulberry32(a) {
     return function() {
       let t = a += 0x6D2B79F5;
-      t = Math.imul(t ^ t >>> 15, t | 1);
-      t ^= t + Math.imul(t ^ t >>> 7, t | 61);
-      return ((t ^ t >>> 14) >>> 0) / 4294967296;
+      t = Math.imul(t ^ (t >>> 15), t | 1);
+      t ^= t + Math.imul(t ^ (t >>> 7), t | 61);
+      return ((t ^ (t >>> 14)) >>> 0) / 4294967296;
     }
 }
 
