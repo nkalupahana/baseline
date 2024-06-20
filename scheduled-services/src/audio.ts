8ce3a26c858061cc894cd57b6407ffc8858736d6
nkalupahana <avishkank@gmail.com>
2024-06-20 14:01:20 -0700

    Add audio conversion for transcription process

--- scheduled-services/src/audio.ts
+++ scheduled-services/src/audio.ts
@@ -11,6 +11,24 @@ interface ProcessAudioMessage {
     encryptionKey: string;
 }
 
+const MAX_FILE_SIZE = 26_214_400;
+const STARTING_BITRATE = 96;
+
+const convertToOgg = (input: string, output: string, bitrate: number) => {
+    console.log("Convert at bitrate " + bitrate);
+    return new Promise((res, rej) => {
+        ffmpeg(input)
+            .noVideo()
+            .outputOptions('-map_metadata', '-1')
+            .audioChannels(1)
+            .audioCodec('libopus')
+            .audioBitrate(`${bitrate}k`)
+            .save(output)
+            .on('end', res)
+            .on('error', rej);
+    });
+}
+
 export const processAudio = async (req: Request, res: Response) => {
     const data = req.body.message.data;
     const body = JSON.parse(Buffer.from(data, "base64").toString("utf-8")) as ProcessAudioMessage;
@@ -19,19 +37,49 @@ export const processAudio = async (req: Request, res: Response) => {
     const outputFilepath = `/tmp/${id}.ogg`;
 
     await getStorage().bucket().file(body.file).download({ destination: inputFilepath });
-    await new Promise((res, rej) => {
-        ffmpeg(inputFilepath)
-            .noVideo()
-            .outputOptions('-map_metadata', '-1')
-            .audioChannels(1)
-            .audioCodec('libopus')
-            .audioBitrate('90k')
-            .save(outputFilepath)
-            .on('end', res)
-            .on('error', rej);
-    });
+    await convertToOgg(inputFilepath, outputFilepath, STARTING_BITRATE);
 
-    await getStorage().bucket().file(`tmp/${id}.ogg`).save(fs.readFileSync(outputFilepath), { contentType: "audio/ogg;codecs=opus" });
+    const stats = fs.statSync(outputFilepath);
+    if (stats.size >= MAX_FILE_SIZE) {
+        let bitrate = Math.floor(STARTING_BITRATE / (stats.size / MAX_FILE_SIZE));
+        while (fs.statSync(outputFilepath).size >= MAX_FILE_SIZE || bitrate < 8) {
+            await convertToOgg(inputFilepath, outputFilepath, bitrate);
+            bitrate -= 1; 
+        }
+    }
+
+    let text = "Open to listen (unable to transcribe).";
+    if (fs.statSync(outputFilepath).size < MAX_FILE_SIZE) {
+        const form = new FormData();
+        form.append("file", new Blob([fs.readFileSync(outputFilepath)]));
+        form.append("model", "whisper-1");
+        form.append("prompt", "This is a journal entry on the journaling app baseline. Use correct punctuation.");
+
+        const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
+            method: "POST",
+            headers: {
+                "Content-Type": "multipart/form-data",
+                "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
+            },
+            body: form
+        });
+        
+        if (response.ok) {
+            const json = await response.json();
+            if (json.text) {
+                text = json.text;
+            } else {
+                console.error(json);
+            }
+        } else {
+            console.error(new Error("OPENAI TRANSCRIPTION FAILED"));
+            console.error(await response.text());
+        }
+    }
+
+    console.log(text);
+
+    //await getStorage().bucket().file(`tmp/${id}-upload.ogg`).save(fs.readFileSync(`/tmp/${id}-upload.ogg`), { contentType: "audio/ogg;codecs=opus" });
     
     res.sendStatus(200);
 };
