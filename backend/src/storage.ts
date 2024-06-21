e4098d916fd3e22ddbcdb645261a78c681051ef5
nkalupahana <avishkank@gmail.com>
2024-06-21 13:05:16 -0700

    Encode MP3 buffer correctly

--- backend/src/storage.ts
+++ backend/src/storage.ts
@@ -77,5 +77,5 @@ export const getAudio = async (req: UserRequest, res: Response) => {
     }
 
     res.setHeader("Content-Type", "audio/mp3");
-    res.send(Buffer.from(AES.decrypt(file[0].toString("utf8"), encryptionKey).toString(), "base64"));
+    res.send(Buffer.from(AES.decrypt(file[0].toString("utf8"), encryptionKey).toString(aesutf8), "base64"));
 }
