757f268ed04095001ba0b2dfe44f151fe8565e3d
nkalupahana <avishkank@gmail.com>
2024-06-20 22:11:32 -0700

    Write transcript and audio to database/storage

--- scheduled-services/src/audio.ts
+++ scheduled-services/src/audio.ts
@@ -3,6 +3,9 @@ import { getStorage } from "firebase-admin/storage";
 import { v4 as uuidv4 } from "uuid";
 import ffmpeg from "fluent-ffmpeg";
 import fs from "node:fs";
+import AES from "crypto-js/aes.js";
+import { getDatabase } from "firebase-admin/database";
+import aesutf8 from "crypto-js/enc-utf8.js";
 
 interface ProcessAudioMessage {
     user: string;
@@ -30,6 +33,7 @@ const convertToOgg = (input: string, output: string, bitrate: number) => {
 }
 
 export const processAudio = async (req: Request, res: Response) => {
+    // Get data
     const data = req.body.message.data;
     const body = JSON.parse(Buffer.from(data, "base64").toString("utf-8")) as ProcessAudioMessage;
     const id = uuidv4();
@@ -37,8 +41,11 @@ export const processAudio = async (req: Request, res: Response) => {
     const outputFilepath = `/tmp/${id}.ogg`;
 
     await getStorage().bucket().file(body.file).download({ destination: inputFilepath });
+
+    // Convert to starting bitrate ogg
     await convertToOgg(inputFilepath, outputFilepath, STARTING_BITRATE);
 
+    // If too big, calculate new bitrate and get to acceptable size
     const stats = fs.statSync(outputFilepath);
     if (stats.size >= MAX_FILE_SIZE) {
         let bitrate = Math.floor(STARTING_BITRATE / (stats.size / MAX_FILE_SIZE));
@@ -48,17 +55,17 @@ export const processAudio = async (req: Request, res: Response) => {
         }
     }
 
+    // Transcribe audio
     let text = "Open to listen (unable to transcribe).";
     if (fs.statSync(outputFilepath).size < MAX_FILE_SIZE) {
         const form = new FormData();
-        form.append("file", new Blob([fs.readFileSync(outputFilepath)]));
+        form.append("file", new Blob([fs.readFileSync(outputFilepath)], { type: "audio/ogg" }));
         form.append("model", "whisper-1");
         form.append("prompt", "This is a journal entry on the journaling app baseline. Use correct punctuation.");
 
         const response = await fetch("https://api.openai.com/v1/audio/transcriptions", {
             method: "POST",
             headers: {
-                "Content-Type": "multipart/form-data",
                 "Authorization": `Bearer ${process.env.OPENAI_API_KEY}`
             },
             body: form
@@ -69,17 +76,44 @@ export const processAudio = async (req: Request, res: Response) => {
             if (json.text) {
                 text = json.text;
             } else {
+                console.error(new Error("OPENAI TRANSCRIPTION FAILED - 1"));
                 console.error(json);
             }
         } else {
-            console.error(new Error("OPENAI TRANSCRIPTION FAILED"));
+            console.error(new Error("OPENAI TRANSCRIPTION FAILED - 2"));
             console.error(await response.text());
         }
     }
 
-    console.log(text);
+    // Get MP3 for upload
+    await new Promise((res, rej) => {
+        ffmpeg(inputFilepath)
+            .noVideo()
+            .outputOptions('-map_metadata', '-1')
+            .audioChannels(1)
+            .audioBitrate(`96k`)
+            .save(`/tmp/${id}-upload.mp3`)
+            .on('end', res)
+            .on('error', rej);
+    });
 
-    //await getStorage().bucket().file(`tmp/${id}-upload.ogg`).save(fs.readFileSync(`/tmp/${id}-upload.ogg`), { contentType: "audio/ogg;codecs=opus" });
+    // Encrypt and upload
+    const mp3 = fs.readFileSync(`/tmp/${id}-upload.mp3`);
+    const mp3Encrypted = AES.encrypt(mp3.toString("base64"), body.encryptionKey).toString();
+    await getStorage().bucket().file(`user/${body.user}/${id}.mp3.enc`).save(mp3Encrypted);
+
+    // Update log
+    const db = getDatabase();
+    const ref = db.ref(`/${body.user}/logs/${body.log}`);
+    let logData = await (await ref.get()).val();
+    logData = JSON.parse(AES.decrypt(logData.data, body.encryptionKey).toString(aesutf8));
+    logData.audio = id;
+    logData.text = text;
+
+    await ref.set({
+        data: AES.encrypt(JSON.stringify(logData), body.encryptionKey).toString()
+    });
+    await db.ref(`/${body.user}/offline`).set(Math.random());
     
     res.sendStatus(200);
 };
