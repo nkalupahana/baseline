18d9824925bb54505f7f10456ba2b40d9b75a8b6
nkalupahana <avishkank@gmail.com>
2024-06-25 13:46:36 -0700

    Reduce code duplication

--- backend/src/storage.ts
+++ backend/src/storage.ts
@@ -5,7 +5,7 @@ import { getDatabase } from "firebase-admin/database";
 import { getStorage } from "firebase-admin/storage";
 import aesutf8 from "crypto-js/enc-utf8.js";
 
-export const getImage = async (req: UserRequest, res: Response) => {
+const getFile = async (req: UserRequest, res: Response) => {
     const db = getDatabase();
 
     const body = req.body;
@@ -16,7 +16,7 @@ export const getImage = async (req: UserRequest, res: Response) => {
     }
 
     const filename = body.filename;
-    // Max length: 32-char UUID.webp.enc
+    // Max length: 32-char UUID.extn.enc
     if (typeof filename !== "string" || filename.length > 45) {
         res.send(400);
         return;
@@ -37,6 +37,15 @@ export const getImage = async (req: UserRequest, res: Response) => {
         return;
     }
 
+    return { file, filename, encryptionKey };
+};
+
+export const getImage = async (req: UserRequest, res: Response) => {
+    const result = await getFile(req, res);
+    if (!result) return;
+
+    const { file, filename, encryptionKey } = result;
+    
     if (filename.endsWith(".enc")) {
         res.send(AES.decrypt(file[0].toString("utf8"), encryptionKey).toString(aesutf8));
     } else {
@@ -45,36 +54,10 @@ export const getImage = async (req: UserRequest, res: Response) => {
 }
 
 export const getAudio = async (req: UserRequest, res: Response) => {
-    const db = getDatabase();
+    const result = await getFile(req, res);
+    if (!result) return;
 
-    const body = req.body;
-    const encryptionKey = await validateKeys(body.keys, db, req.user!.user_id);
-    if (!encryptionKey) {
-        res.send(400);
-        return;
-    }
-
-    const filename = body.filename;
-    // Max length: 32-char UUID.mp3.enc
-    if (typeof filename !== "string" || filename.length > 45) {
-        res.send(400);
-        return;
-    }
-
-    let file;
-    try {
-        file = await getStorage().bucket().file(`user/${req.user!.uid}/${filename}`).download();
-    } catch {
-        // File doesn't exist, fail
-        res.send(400);
-        return;
-    }
-
-    // Null check for type safety
-    if (!file) {
-        res.send(400);
-        return;
-    }
+    const { file, encryptionKey } = result;
 
     res.setHeader("Content-Type", "audio/mp3");
     res.send(Buffer.from(AES.decrypt(file[0].toString("utf8"), encryptionKey).toString(aesutf8), "base64"));
