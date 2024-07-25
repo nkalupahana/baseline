b974f8cb2fe4cb816aed4d321589df67e3f8577b
nkalupahana <avishkank@gmail.com>
2024-07-25 12:49:49 -0400

    Add refresher to try to fix Dexie rejection

--- src/lifecycle.ts
+++ src/lifecycle.ts
@@ -1,5 +1,6 @@
 import { checkKeys, parseSettings } from "./helpers";
 import history from "./history";
+import * as Sentry from "@sentry/react";
 
 const THRESHOLD_SEC = 60 * 3;
 
@@ -29,6 +30,16 @@ document.addEventListener("visibilitychange", () => {
     if (parseSettings()["pdp"] && (window.location.pathname !== "/journal/finish" || window.location.hash !== "#attach")) {
         sessionStorage.removeItem("pwd");
         window.location.href = "/rsummary";
+    }
+});
+
+window.addEventListener("unhandledrejection", (event) => {
+    if (event.reason.message.toLowerCase().includes("refresh")) {
+        console.log("Unhandled rejection, refreshing. (1)");
+        Sentry.addBreadcrumb({
+            message: "Unhandled rejection, refreshing. (2)"
+        });
+        Sentry.flush(1000).then(() => window.location.reload());
     }
 });
 
