4a60c9cbdeffc8809128d2e09c1e4fc7139aa38a
nkalupahana <avishkank@gmail.com>
2024-11-01 22:22:40 -0700

    productionize

--- src/pages/WrappedSummary.tsx
+++ src/pages/WrappedSummary.tsx
@@ -2,6 +2,7 @@ import React from "react";
 import Summary from "./Summary";
 import Preloader from "./Preloader";
 import * as Sentry from "@sentry/react";
+import { ONE_MINUTE } from "../components/graphs/helpers";
 
 interface Props {}
 
@@ -21,20 +22,23 @@ class WrappedSummary extends React.Component<Props, State> {
 
     componentDidCatch(error: Error) {
         console.log("Error, likely from fetching data from Dexie (caught). WRAPPED");
-        let info = "";
+        let info = "Caught error in Wrapped Summary! ";
         try {
             info += `name: ${error.name}, message: ${error.message}`;
         } catch {
             info += `Could not get error data. ${error}`;
         }
 
-        alert(`IndexedDB error, refreshing. ${info}`);
-
-        Sentry.addBreadcrumb({
-            category: "IndexedDB",
-            message: "Error, refreshing!"
-        });
-        window.location.reload();
+        Sentry.captureException(new Error(info));
+        const lastRefresh = localStorage.getItem("lastRefresh");
+        if (!lastRefresh || Date.now() - parseInt(lastRefresh) > ONE_MINUTE) {
+            localStorage.setItem("lastRefresh", Date.now().toString());
+            window.location.reload();
+        } else {
+            Sentry.captureException(new Error("Critical error in WrappedSummary, reloading did not fix the issue."));
+            Sentry.captureException(error);
+            alert("baseline has run into a critical error. Please force quit and reopen the app. If the issue persists, contact us at hello@getbaseline.app.");
+        }
     }
 
     render() {
