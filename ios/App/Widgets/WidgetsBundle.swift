e4c09cff9a04abe33d15724ef93797d8d69c4a8d
nkalupahana <avishkank@gmail.com>
2025-01-17 07:25:23 +0530

    Sample widget created + building + can get ID token

---
+++ ios/App/Widgets/WidgetsBundle.swift
@@ -0,0 +1,16 @@
+//
+//  WidgetsBundle.swift
+//  Widgets
+//
+//  Created by Nisala on 1/17/25.
+//
+
+import WidgetKit
+import SwiftUI
+
+@main
+struct WidgetsBundle: WidgetBundle {
+    var body: some Widget {
+        StreakWidget()
+    }
+}
