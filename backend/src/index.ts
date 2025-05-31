1ea0f8d0dcc5c3454f500e1627aa6e45652dcfbf
kyleburgess <kylie.m.burgess@vanderbilt.edu>
2025-05-30 17:40:24 -0700

    Sorry

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -2,7 +2,7 @@ import express from "express";
 import { initializeApp } from "firebase-admin/app";
 import cors from "cors";
 import * as Sentry from "@sentry/node";
-import { UserRequest } from "./helpers.js";
+import { checkQuota, UserRequest, validateAuth } from "./helpers.js";
 import { changePDPpassphrase, disablePDP, enablePDP } from "./pdp.js";
 import { deleteAccount, getOrCreateKeys, sync } from "./accounts.js";
 import { moodLog, survey } from "./main.js";
@@ -14,41 +14,38 @@ import { calculateStreak } from "./streak.js";
 
 const app = express();
 initializeApp({
-  databaseURL: "https://getbaselineapp-default-rtdb.firebaseio.com/",
-  storageBucket: "getbaselineapp.appspot.com",
+    databaseURL: "https://getbaselineapp-default-rtdb.firebaseio.com/",
+    storageBucket: "getbaselineapp.appspot.com"
 });
 
 Sentry.init({
-  dsn: "https://4fb379f6ae1a4e569ae826f066adf8ba@o4504179120472064.ingest.sentry.io/4504179121717248",
-  integrations: [...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()],
-  tracesSampleRate: 1.0,
+    dsn: "https://4fb379f6ae1a4e569ae826f066adf8ba@o4504179120472064.ingest.sentry.io/4504179121717248",
+    integrations: [
+        ...Sentry.autoDiscoverNodePerformanceMonitoringIntegrations()
+    ],
+    tracesSampleRate: 1.0,
 });
 
 app.use(cors());
 app.use(Sentry.Handlers.requestHandler());
 app.use(Sentry.Handlers.tracingHandler());
 app.use(async (req: UserRequest, res, next) => {
-  if (req.path.startsWith("/analytics/")) return next();
-
-  await validateAuth(req, res);
-  if (req.user && await checkQuota(req, res)) {
-      next();
-  }
+    if (req.path.startsWith("/analytics/")) return next();
+    
+    await validateAuth(req, res);
+    if (req.user && await checkQuota(req, res)) {
+        next();
+    }
 });
 
-app.use(
-  express.json({
+app.use(express.json({
     type(req) {
-      return (
-        req.headers["content-type"] === "application/json" ||
-        !req.headers["content-type"]
-      );
-    },
-  })
-);
+        return req.headers["content-type"] === "application/json" || !req.headers["content-type"];
+    }
+}));
 
 app.get("/", (_, res) => {
-  res.status(200).send("baseline API up and running.");
+    res.status(200).send("baseline API up and running.");
 });
 
 // PDP
@@ -78,5 +75,5 @@ app.use(Sentry.Handlers.errorHandler());
 
 const port = process.env.PORT || 8080;
 app.listen(port, () => {
-  console.log("Listening on port", port);
+    console.log("Listening on port", port);
 });
