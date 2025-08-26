612e4ad5e2cc7be9541d4e8f595d17de7eab0262
Nisala <nisala.kalupahana@neuralink.com>
2025-08-26 00:48:32 -0700

    compress video

--- cypress.config.ts
+++ cypress.config.ts
@@ -4,6 +4,7 @@ export default defineConfig({
   e2e: {
     testIsolation: false,
     video: true,
+    videoCompression: true,
     setupNodeEvents(on, config) {
       return require("./cypress/plugins/index.js")(on, config);
     },
